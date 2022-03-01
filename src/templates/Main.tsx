7ea614e72745516b8833cac95a15dc2e47061195
Lee <lgdelrosario@outlook.com>
2022-03-02 02:23:21 +0800

    cleanup and add structure

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -1,8 +1,7 @@
 import { ReactNode } from 'react';
 
-import Link from 'next/link';
-
-import { AppConfig } from '@/utils/AppConfig';
+import Footer from '@/components/Footer/Footer';
+import Navigation from '@/components/Navigation/Navigation';
 
 type IMainProps = {
   meta: ReactNode;
@@ -10,60 +9,13 @@ type IMainProps = {
 };
 
 const Main = (props: IMainProps) => (
-  <div className="px-1 w-full antialiased text-gray-700">
+  <div className="antialiased">
     {props.meta}
 
-    <div className="mx-auto max-w-screen-md">
-      <div className="border-b border-gray-300">
-        <div className="pt-16 pb-8">
-          <div className="text-3xl font-bold text-gray-900">
-            {AppConfig.title}
-          </div>
-          <div className="text-xl">{AppConfig.description}</div>
-        </div>
-        <div>
-          <ul className="flex flex-wrap text-xl">
-            <li className="mr-6">
-              <Link href="/">
-                <a className="text-gray-700 hover:text-gray-900 border-none">
-                  Home
-                </a>
-              </Link>
-            </li>
-            <li className="mr-6">
-              <Link href="/about/">
-                <a className="text-gray-700 hover:text-gray-900 border-none">
-                  About
-                </a>
-              </Link>
-            </li>
-            <li className="mr-6">
-              <a
-                className="text-gray-700 hover:text-gray-900 border-none"
-                href="https://github.com/ixartz/Next-js-Boilerplate"
-              >
-                GitHub
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <div className="py-5 text-xl content">{props.children}</div>
-
-      <div className="py-8 text-sm text-center border-t border-gray-300">
-        © Copyright {new Date().getFullYear()} {AppConfig.title}. Powered with{' '}
-        <span role="img" aria-label="Love">
-          ♥
-        </span>{' '}
-        by <a href="https://creativedesignsguru.com">CreativeDesignsGuru</a>
-        {/*
-         * PLEASE READ THIS SECTION
-         * We'll really appreciate if you could have a link to our website
-         * The link doesn't need to appear on every pages, one link on one page is enough.
-         * Thank you for your support it'll mean a lot for us.
-         */}
-      </div>
+    <div>
+      <Navigation />
+      <div>{props.children}</div>
+      <Footer />
     </div>
   </div>
 );
